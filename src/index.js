854352f559805857963c9c8e653bac166730d7e9
Sudeep Yegnashankaran <sudeepy@gmail.com>
2021-07-29 17:56:27 -0700

    adding auth0 and login button

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-ql1kxgm5.us.auth0.com"
+      clientId="MCgAXU791OdMQnzhcpOudJuFRMhn5CkE"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
