fc7519fec6db51b42cb1bad4e36c9fb406c405da
Sudeep Yegnashankaran <sudeepy@gmail.com>
2021-07-29 18:00:41 -0700

    rendering basic profile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginButton from './LoginButton.js';
+import Profile from './Profile.js';
 
 function App() {
   return (
     <div className="App">
       <LoginButton />
+      <Profile />
     </div>
   );
 }
